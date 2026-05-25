11b55c00d7c2e6d693399896e1defaed1c9586a5
meetsheth07 <meetsheth698@gmail.com>
2026-05-25 19:12:13 +0530

    Add 11_THIS&ARRAOWfunction.js

---
+++ 11_THIS&ARRAOWfunction.js
@@ -0,0 +1,34 @@
+const user = {
+    name:"Dave",
+    age: 28,
+    email: "dave@example.com",
+    welcomeMessage: function() {
+        console.log(`Welcome, ${this.name}!`);
+        console.log(this); 
+    }   
+}
+console.log(user);
+user.welcomeMessage(); 
+
+// user.name = "David";
+// user.welcomeMessage();
+console.log(this);
+
+const chai = function() {
+    let username = "Charlie";
+    console.log(`Hello, ${this.username}!`);
+    console.log(this); 
+}
+
+chai();
+
+
+//arrow  function
+const arrowFunc = (username) => {
+    console.log(`Hello, ${username}!`);
+    console.log(this); 
+}
+arrowFunc("Charlie");
+
+const addtwo = (a, b) => a + b;
+console.log(addtwo(5, 3));
